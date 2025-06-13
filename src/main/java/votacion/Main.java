6e73d30cf43255d50f5bc96a6fdef0d7b1d5be7b
MateoVanden <mateovandenp@gmail.com>
2025-06-13 20:31:46 -0300

    segundo commit

--- src/main/java/votacion/Main.java
+++ src/main/java/votacion/Main.java
@@ -2,6 +2,11 @@ package votacion;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SistemaVotacion sistema = new SistemaVotacion(new Notificador());
+        Votante v1 = new Votante("123", "Ana");
+        Votante v2 = new Votante("456", "Luis");
+
+        sistema.votar(v1);
+        sistema.votar(v2);
     }
 }
